ad5caa1f8648c3e0bfe5b5fee53da156b0280f0c
Eduardo Rasgado Ruiz <eduardo.rasgado@hotmail.com>
2019-11-20 03:59:31 -0600

    revision de los componentes principales del proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // etiqueta html q se usa para incluir en el html
   selector: 'app-root',
   templateUrl: './app.component.html',
+  // estilos de solo este componente, si son varios se separan por comas
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Oval-cliente';
+  title = 'Oval Store';
+
+  curso: string = 'Curso de manejo de Angular 7/8 con Spring 5';
+  profesor: string = 'Andrés Guzmán de Udemy';
+  alumno: string = 'Eduardo Rasgado Ruiz';
 }
